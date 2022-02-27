7913108b2fa31e79df3b661471eb233dd7287a0c
Alejandro Cazorla <acazorla72@gmail.com>
2022-02-27 18:58:56 +0100

    minor change

--- pages/shop.js
+++ pages/shop.js
@@ -1,6 +1,7 @@
 import React from "react";
 
-function shop() {
+function shop({ articles }) {
+  console.log(articles);
   return (
     <div>
       <h2>Here is your shop</h2>
@@ -8,5 +9,18 @@ function shop() {
     </div>
   );
 }
+
+export const getStaticProps = async () => {
+  const res = await fetch(
+    `https://jsonplaceholder.typicode.com/posts?_limit=4`
+  );
+  const articles = await res.json();
+
+  return {
+    props: {
+      articles,
+    },
+  };
+};
 
 export default shop;
