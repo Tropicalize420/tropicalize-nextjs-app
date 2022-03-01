2ac974c848c7a1507f699666aa888e90df5d534d
ChabaJosa <chabagjg@gmail.com>
2022-02-28 22:14:35 -0500

    Added modified base navbar (not responsive)

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,30 +1,81 @@
 import Link from "next/link";
-import React from "react";
+import React, { useState } from "react";
 import navStyles from "../styles/Navbar.module.css";
-
+import { Input, Menu } from "semantic-ui-react";
+//
 function Navbar() {
+  //
+  const [activeItem, setActiveItem] = useState(null);
+  //
   return (
-    <div className={navStyles.navDiv}>
-      <nav className={navStyles.nav}>
-        <ul className={navStyles.navUl}>
-          <li>
-            <Link href="/">Home</Link>
-          </li>
-          <li>
-            <Link href="/products">Products</Link>
-          </li>
-          <li>
-            <Link href="/animations">Animations</Link>
-          </li>
-          <li>
-            <Link href="/shop">Shop</Link>
-          </li>
-          <li>
-            <Link href="/map">Map</Link>
-          </li>
-        </ul>
-      </nav>
-    </div>
+    <nav className={navStyles.nav}>
+      <Menu secondary>
+        <Link href="/" passHref>
+          <Menu.Item
+            name="Home"
+            active={activeItem === "Home"}
+            onClick={(e) => {
+              setActiveItem(e.target.name);
+            }}
+          >
+            Home
+          </Menu.Item>
+        </Link>
+        <Link href="/products" passHref>
+          <Menu.Item
+            name="Products"
+            active={activeItem === "Products"}
+            onClick={(e) => {
+              setActiveItem(e.target.name);
+            }}
+          >
+            Products
+          </Menu.Item>
+        </Link>
+        <Link href="/map" passHref>
+          <Menu.Item
+            name="Map"
+            active={activeItem === "Map"}
+            onClick={(e) => {
+              setActiveItem(e.target.name);
+            }}
+          >
+            Map
+          </Menu.Item>
+        </Link>
+        <Link href="/animations" passHref>
+          <Menu.Item
+            name="Animations"
+            active={activeItem === "Animations"}
+            onClick={(e) => {
+              setActiveItem(e.target.name);
+            }}
+          >
+            Animations
+          </Menu.Item>
+        </Link>
+        <Link href="/shop" passHref>
+          <Menu.Item
+            name="shop"
+            active={activeItem === "shop"}
+            onClick={(e) => {
+              setActiveItem(e.target.name);
+            }}
+          >
+            Shop
+          </Menu.Item>
+        </Link>
+        <Menu.Menu position="right">
+          <Menu.Item>
+            <Input
+              icon="search"
+              color="rgb(252, 77, 158)"
+              placeholder="Search Products..."
+            />
+          </Menu.Item>
+        </Menu.Menu>
+      </Menu>
+    </nav>
   );
 }
 
