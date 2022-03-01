5cdb1cba3cfdbda709fba590379d4a295643cdc5
ChabaJosa <chabagjg@gmail.com>
2022-02-28 21:48:49 -0500

    Added some of the content from previous react frontend but css isnt picking up the parallax

--- pages/index.js
+++ pages/index.js
@@ -1,45 +1,78 @@
-import Head from "next/head";
-import Image from "next/image";
 import styles from "../styles/Home.module.css";
-import { useContext, useEffect } from "react";
+import { useContext } from "react";
 import { Context } from "../context/AppProvider";
-import axios from "axios";
+import Buildings from "../public/elementscrop.png";
+import Tropi from "../public/tropi_city.png";
+import Image from "next/image";
 //
-export default function Home({ res }) {
+export default function Home() {
   //
-  const { state, getProducts } = useContext(Context);
-  useEffect(() => {
-    getProducts(res);
-  }, []);
-  //
-  console.log("State ---> ", state);
-  console.log("RES ---> ", res);
+  const { state } = useContext(Context);
+  console.log(state);
   //
   return (
-    <div className={styles.container}>
-      <h2>This is home page</h2>
-      <p>... more text</p>
+    <div className={styles.main_container}>
+      <div className={styles.container_div}>
+        <div className={styles.wrapper}>
+          <section className={styles.section}>
+            <Image
+              src={Tropi}
+              alt="Img Here"
+              className={styles.foreground}
+              // src="https://res.cloudinary.com/alecos008/image/upload/v1644504881/Web_Cover_kfhrww.png"
+            />
+            <Image
+              src={Buildings}
+              alt="Img Here"
+              className={styles.background}
+              style={{ minHeight: "150vh" }}
+            />
+
+            <h2 className={styles.title}>Welcome to Tropicalize's Landing Page</h2>
+          </section>
+        </div>
+        <article className={styles.article}>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse
+            facilis unde illo neque at hic quisquam, nulla consectetur, itaque
+            maxime sit iusto aliquam culpa reiciendis eaque recusandae vitae
+            modi, animi expedita perferendis inventore. Culpa incidunt odit
+            dicta quibusdam ullam facilis eum magnam, consectetur voluptates
+            praesentium, pariatur eos nihil! Natus in, autem aut facere repellat
+            eveniet commodi illo ullam hic fugit deserunt explicabo esse atque
+            reprehenderit optio, dicta suscipit sed! Porro fugiat numquam vel
+            magni, iure provident consequuntur velit ut quia itaque repudiandae
+            molestiae, nobis sequi incidunt autem! Cum impedit accusamus
+            laboriosam eaque necessitatibus consectetur incidunt a
+            exercitationem molestias doloribus tenetur amet, ullam at
+            repudiandae, quos reprehenderit adipisci assumenda dolor enim
+            voluptas natus! Assumenda praesentium voluptate culpa quidem quasi
+            impedit unde incidunt odit velit, dolore officiis distinctio, fugit,
+            optio sed. Quos nesciunt eius reprehenderit ipsa consectetur
+            tempora, fugiat provident corporis perspiciatis veritatis totam
+            temporibus voluptates soluta vitae mollitia molestias reiciendis
+            expedita doloribus est. Maxime et fugit expedita id sed cumque harum
+            dignissimos aperiam voluptatum quos obcaecati illo tempore
+            consectetur, sunt perferendis eius aliquid soluta praesentium
+            voluptatem assumenda ex fuga. Iusto, possimus? Dolores deleniti
+            eaque, aperiam consequuntur odit culpa, a rchitecto, quas veniam
+            provident inventore eius. Laboriosam in reprehenderit quidem?
+            Officiis illum molestiae, praesentium velit, illo debitis quasi fuga
+            laborum veritatis necessitatibus fugiat cupiditate! Quia animi
+            provident maxime numquam porro itaque! Provident, rerum. Aliquid cum
+            quo ducimus ad? Repellat nihil, saepe beatae rerum sint eveniet
+            voluptate officiis velit autem officia tempore deserunt sequi quod
+            quae culpa sed. Reprehenderit eveniet ipsum nihil ipsam quam
+            facilis, possimus modi placeat ratione soluta voluptate obcaecati
+            error minima enim, fugiat quaerat architecto, iure dignissimos a eum
+            quidem eos at fuga. Consectetur, quisquam at reprehenderit in
+            exercitationem inventore incidunt dolorum quos adipisci labore?
+            Fugiat facilis ipsum minus, officiis eos modi sequi distinctio
+            pariatur reprehenderit accusantium labore obcaecati, recusandae
+            saepe?
+          </p>
+        </article>
+      </div>
     </div>
   );
-}
-
-export async function getServerSideProps() {
-  //
-  //   .get(`${process.env.REACT_APP_API_HOST}/products/all`, {
-  // const data = await req.json();
-  const data = await axios
-    .get("http://localhost:5005/api/products/all", {
-      withCredentials: true,
-    })
-    .then((response) => {
-      console.log("Api Response From Context ---> ", response);
-      return [...response.data.products];
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-  //
-  return {
-    props: { res: data },
-  };
 }
--- pages/products/index.js
+++ pages/products/index.js
@@ -1,52 +1,130 @@
-import React, { useRef, useContext } from "react";
+import React, { useRef, useContext, useEffect } from "react";
 import { useRouter } from "next/router";
-import { Input, Button, Header, Icon, Segment } from "semantic-ui-react";
+import { Input, Button, Header, Icon, Image } from "semantic-ui-react";
 import styles from "../../styles/Products.module.css";
 import { Context } from "../../context/AppProvider";
+import axios from "axios";
 //
-export default function ProductList() {
+export default function ProductList({ res }) {
   //
-  const { state } = useContext(Context);
-  console.log('Hello from products! Heres the state I have from context ---> ',state)
+  const { state, getProducts, generateCartFromStorage } = useContext(Context);
+  useEffect(() => {
+    getProducts(res);
+    generateCartFromStorage({
+      cart: {
+        white: {
+          _id: "61aba1622e2b3c45099951e9",
+          name: "Tropicalize Silicone-Coated White Grinder",
+          image:
+            "https://res.cloudinary.com/alecos008/image/upload/v1635680349/White_Grinder_cm7vca.jpg",
+          quantity: 0,
+          type: "white",
+        },
+        black: {
+          _id: "61aba0e02e2b3c45099951e8",
+          name: "Tropicalize Silicone-Coated Black Grinder",
+          image:
+            "https://res.cloudinary.com/alecos008/image/upload/v1635680327/Black_Grinder_gyyzr5.jpg",
+          quantity: 0,
+          type: "black",
+        },
+        piney: {
+          _id: "61dd6e6de8e6c9ece63c93ec",
+          name: "Tropicalize Small Piney Bio-Grinder",
+          image:
+            "https://res.cloudinary.com/alecos008/image/upload/v1641850646/ALL_yklipo.png",
+          quantity: 0,
+          type: "piney",
+        },
+        tropi: {
+          _id: "61dd6eeae8e6c9ece63c93ed",
+          name: "Tropicalize Small Tropi Bio-Grinder",
+          image:
+            "https://res.cloudinary.com/alecos008/image/upload/v1641850720/All_sk8uml.png",
+          quantity: 0,
+          type: "tropi",
+        },
+      },
+    });
+  }, []);
+  console.log("Hello from products! ---> ", state);
+  //
   const input = useRef("");
   const router = useRouter();
   //
-  return (
-    <div className={styles.container}>
-      <Header as="h2">
-        <Icon name="search" style={{ backgroundColor: '#FF4C9D"' }} />
-        <Header.Content>
-          Search
-          <Header.Subheader>For Products</Header.Subheader>
-        </Header.Content>
-      </Header>
+  if (state.products !== undefined) {
+    //&& state.cart !== undefined
+    return (
+      <div className={styles.product_screen}>
+        <div className={`${styles.figure_container} ${styles.mobile_hidden}`}>
+          <Image
+            alt="Img "
+            src="https://res.cloudinary.com/alecos008/image/upload/v1642968688/Logo_copy_tc63hp.png"
+            size="big"
+          />
+        </div>
 
-      <Input
-        // fluid
-        style={{ maxWidth: "50%" }}
-        focus
-        placeholder="Search..."
-        onChange={(e) => {
-          input.current = e.target.value;
-        }}
-      />
-      <Button
-        // fluid
-        style={{
-          margin: "1rem 0",
-          backgroundColor: "#FF4C9D",
-          color: "white",
-          maxWidth: "50%",
-        }}
-        onClick={() => {
-          router.push(`/domains/${input.current}`);
-        }}
-      >
-        Search
-      </Button>
-      <div className={styles.img}>
-        {/* <Image src="/1.png" alt="me" width={1000} height={500} /> */}
+        <div className={[styles.products_container, styles.desktop_scrollable]}>
+          {state.products.map((product) => {
+            return (
+              <div className={styles.product_card} key={product._id}>
+                <h2>{product.name}</h2>
+                <div>
+                  <img
+                    src={product.image1}
+                    className="product-img"
+                    alt="Grinder"
+                  />
+                </div>
+                <button
+                  // onClick={() => addOne(state.cart, product.type)}
+                  className={styles.product_btn}
+                >
+                  {/* {state.cart[product.type].quantity > 0
+                    ? "Item added - Select Quantity in Cart"
+                    : "Add Item "} */}
+                </button>
+              </div>
+            );
+          })}
+        </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    return (
+      <>
+        <h1
+          style={{
+            minHeight: "90vh",
+            minWidth: "90vw",
+            textAlign: "center",
+            textAlignVertical: "center",
+          }}
+        >
+          Loading ...{" "}
+        </h1>
+      </>
+    );
+  }
+}
+
+export async function getServerSideProps() {
+  //
+  //   .get(`${process.env.REACT_APP_API_HOST}/products/all`, {
+  // const data = await req.json();
+  const data = await axios
+    .get("http://localhost:5005/api/products/all", {
+      withCredentials: true,
+    })
+    .then((response) => {
+      console.log("Api Response From Context ---> ", response);
+      return [...response.data.products];
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+  //
+  return {
+    props: { res: data },
+  };
 }
